9a200340080011790d6614ac793c33bb0bb4d87f
julklingel <julklingel@gmail.com>
2024-08-06 14:37:47 +0200

    fixing the signup

--- src/routes/(landing)/components/header-section.svelte
+++ src/routes/(landing)/components/header-section.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import DarkMode from '../../components/ui/darkmode.svelte';
-	import Button from '../../components/ui/button.svelte';
-	export let buttonText = 'Sign Up';
+	import { Button } from "$lib/components/ui/button";
+
     export let variant: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" = "link";
 </script>
 
 <div class="absolute right-0 m-2">
 	<div class="flex">
-		<Button {buttonText} {variant} />
+		<Button {variant}> Signup </Button>
 		<DarkMode />
 	</div>
 </div>
