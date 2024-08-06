b87b6470d7afe0c1cbcb3f0d4c60dde17363ee86
julklingel <julklingel@gmail.com>
2024-08-06 14:28:46 +0200

    exchanged menu button

--- src/routes/(landing)/components/title-section.svelte
+++ src/routes/(landing)/components/title-section.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
-	import DarkMode from '../../components/ui/darkmode.svelte';
-	import Button from '../../components/ui/button.svelte';
+    import DarkMode from '../../components/ui/darkmode.svelte';
+    import { Button } from "$lib/components/ui/button";
     import { goto } from '$app/navigation';
-	export let buttonText = 'Login';
-    export let variant: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" = "default";
-
-	async function navigate() {
-		goto("./menu")
 
 
-	}
-
+    async function navigate() {
+		console.log("Navigating to /menu");
+        await goto("/menu");
+    }
 </script>
 
 <section class="flex flex-col h-screen items-center justify-center">
-	<h1 class="text-4xl font-extrabold tracking-tight lg:text-5xl">Scanlytics</h1>
-	<p class="mt-6 pl-6 italic">#1 Text Editor in der Medizin</p>
+    <h1 class="text-4xl font-extrabold tracking-tight lg:text-5xl">Scanlytics</h1>
+    <p class="mt-6 pl-6 italic">#1 Text Editor in der Medizin</p>
     <br>
-    <Button {buttonText} {variant}/>
-	<button on:click={navigate}>Menu</button>
-
+    <Button on:click={navigate} >Menu </Button>
 </section>
