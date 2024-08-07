86d34e5dd7b10fd29041cb84bf389f4f258b637d
julklingel <julklingel@gmail.com>
2024-08-07 22:33:20 +0200

    fixed patientname issue in patient notes

--- src/routes/components/ui/navigation.svelte
+++ src/routes/components/ui/navigation.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
     import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
-    import { page } from '$app/stores';
+    import { page } from "$app/stores";
     import { PatientStore } from "../../../stores/Patient";
     import { UserStore } from "../../../stores/User";
     import { patientNotes } from "../../../stores/PatientNote";
-    
-
 
     function convertIdToName(id: string, store: any) {
         const item = store.find((item: any) => item.id === id);
@@ -14,29 +12,35 @@
 
     function convertIdtoPateintName(id: string, store: any) {
         const item = store.find((item: any) => item.id === id);
-        console.log("heeereeee", item)   
-        return item ? item.patientName : "Unknown";
+        console.log("Item", item);
+        let patientName = convertIdToName(item.patientId, $PatientStore);
+        return patientName;
+    }
+
+    function capitalizeFirstLetter(string: string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
     }
 
     $: breadcrumb = getBreadcrumb($page.url.pathname);
-
-   $: console.log(breadcrumb[0].name)
 
     function getBreadcrumb(url: string) {
         const parts = url.split("/").filter(Boolean);
         let breadcrumb = parts.map((part, index) => ({
             name: capitalizeFirstLetter(part),
-            href: '/' + parts.slice(0, index + 1).join('/')
+            href: "/" + parts.slice(0, index + 1).join("/"),
         }));
 
         if (breadcrumb[0].name === "Patients") {
-            console.log("Hello Sir")
             if (parts.length > 1) {
                 breadcrumb[1].name = convertIdToName(parts[1], $PatientStore);
             }
         } else if (breadcrumb[0].name === "Notes") {
             if (parts.length > 1) {
-                breadcrumb[1].name = convertIdtoPateintName(parts[1], $patientNotes);
+                console.log("Back at it again", parts[1]);
+                breadcrumb[1].name = convertIdtoPateintName(
+                    parts[1],
+                    $patientNotes,
+                );
             }
         } else if (breadcrumb[0].name === "Users") {
             if (parts.length > 1) {
@@ -44,14 +48,9 @@
             }
         }
         return breadcrumb;
-
-
-
     }
 
-    function capitalizeFirstLetter(string: string) {
-        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-    }
+
 </script>
 
 <Breadcrumb.Root>
