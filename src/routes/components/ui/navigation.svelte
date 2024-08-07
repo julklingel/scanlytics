040d02271d27cfefe2ce0837c29060e2f4b413b5
julklingel <julklingel@gmail.com>
2024-08-07 21:58:58 +0200

    if statement for slug page navigation display

--- src/routes/components/ui/navigation.svelte
+++ src/routes/components/ui/navigation.svelte
@@ -1,16 +1,52 @@
 <script lang="ts">
     import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
     import { page } from '$app/stores';
+    import { PatientStore } from "../../../stores/Patient";
+    import { UserStore } from "../../../stores/User";
+    import { patientNotes } from "../../../stores/PatientNote";
+    
 
+
+    function convertIdToName(id: string, store: any) {
+        const item = store.find((item: any) => item.id === id);
+        return item ? item.name : "Unknown";
+    }
+
+    function convertIdtoPateintName(id: string, store: any) {
+        const item = store.find((item: any) => item.id === id);
+        console.log("heeereeee", item)   
+        return item ? item.patientName : "Unknown";
+    }
 
     $: breadcrumb = getBreadcrumb($page.url.pathname);
 
+   $: console.log(breadcrumb[0].name)
+
     function getBreadcrumb(url: string) {
         const parts = url.split("/").filter(Boolean);
-        return parts.map((part, index) => ({
+        let breadcrumb = parts.map((part, index) => ({
             name: capitalizeFirstLetter(part),
             href: '/' + parts.slice(0, index + 1).join('/')
         }));
+
+        if (breadcrumb[0].name === "Patients") {
+            console.log("Hello Sir")
+            if (parts.length > 1) {
+                breadcrumb[1].name = convertIdToName(parts[1], $PatientStore);
+            }
+        } else if (breadcrumb[0].name === "Notes") {
+            if (parts.length > 1) {
+                breadcrumb[1].name = convertIdtoPateintName(parts[1], $patientNotes);
+            }
+        } else if (breadcrumb[0].name === "Users") {
+            if (parts.length > 1) {
+                breadcrumb[1].name = convertIdToName(parts[1], $UserStore);
+            }
+        }
+        return breadcrumb;
+
+
+
     }
 
     function capitalizeFirstLetter(string: string) {
