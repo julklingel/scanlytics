4d878cf7377c335555088c10481062a553ed12d2
julklingel <julklingel@gmail.com>
2024-08-08 16:23:39 +0200

    fixed read lock issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,7 +15,6 @@ use users::controller::{get_users};
 
 
 
-
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
@@ -23,38 +22,34 @@ fn main() {
             tauri::async_runtime::block_on(async {
                 let db = init_db().await.expect("Failed to initialize database");
                
-                define_db_on_startup(db).await.expect("Failed to define database structure");
+               
+                {
+                    let db_guard = db.read().await;
+                    define_db_on_startup(&*db_guard).await.expect("Failed to define database structure");
+                }
+                
                 app.manage(db);
             });
             Ok(())
-            
-        }
-        )
+        })
         .invoke_handler(tauri::generate_handler![
-
             // Test APIs
             test_db_read,
             test_db_delete,
             test_db_write,
-
             //Notes APIs
             create_patient_note,
             get_patient_notes,
             update_patient_note,
             delete_patient_note,
-
             //Patients APIs
             create_patient,
             get_patients,
             update_patient,
             delete_patient,
-
             // User APIs
             get_users
-
-            
-            
-            ])
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
