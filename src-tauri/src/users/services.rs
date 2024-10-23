0ea5c4e5f797855a28de248c85721df55ad15695
julklingel <julklingel@gmail.com>
2024-10-23 14:40:47 +0200

    create user on signup

--- src-tauri/src/users/services.rs
+++ src-tauri/src/users/services.rs
@@ -1,13 +1,26 @@
-use super::models::UserResponse;
+use super::models;
 use surrealdb::engine::local::Db;
 use surrealdb::Surreal;
 
 
 
-pub async fn get_users_service(db: &Surreal<Db>) -> Result<Vec<UserResponse>, String> {
-    let records: Vec<UserResponse> = db
+pub async fn get_users_service(db: &Surreal<Db>) -> Result<Vec<models::UserResponse>, String> {
+    let records: Vec<models::UserResponse> = db
         .select("User")
         .await
         .map_err(|e| e.to_string())?;
     Ok(records)
+}
+
+
+pub async fn create_user_service(user_record: models::UserRecord, db: &Surreal<Db>) -> Result<models::UserResponse, String> {
+    let user: models::UserResponse = db
+        .create("User")
+        .content(user_record)
+        .await
+        .map_err(|e| e.to_string())?
+        .ok_or_else(|| "Failed to create patient".to_string())?;
+
+    Ok(user)
+
 }
